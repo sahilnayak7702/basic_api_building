fd398b434fce03bdab291bd091c91cd4b68ef753
sahilnayak7702 <nayak.sahil@iitgn.ac.in>
2023-07-29 08:25:14 +0530

    first commit

---
+++ oversimplifiedapis/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for oversimplifiedapis project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oversimplifiedapis.settings')
+
+application = get_asgi_application()
